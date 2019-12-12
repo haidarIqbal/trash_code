9ffb8293fc4b79d6b949823db94948168989690a
Haidar Iqbal <haidariqbal49@gmail.com>
2019-12-13 01:57:51 +0500

    some more changes

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   "mongodb://haidarroot123:ChatPass#extension@docdb-2019-12-12-20-33-13.cluster-cltjtvqusmpi.us-east-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0",
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  { useNewUrlParser: true },
   (err, success) => {
     if (err) {
       console.log(err);
