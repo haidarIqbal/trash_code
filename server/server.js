4c22be3926b6d4431fe310895facc92792fe630f
Haidar Iqbal <haidariqbal49@gmail.com>
2020-02-08 12:48:48 +0500

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
-var userModel = require("../model/user");
+//var userModel = require("../model/user");
 const _ = require("lodash");
 var app = express();
 var port = 3000;
@@ -16,84 +16,80 @@ app.use(
     extended: true
   })
 );
-app.post("/login", (req, res) => {
-  var body = _.pick(req.body, ["email", "password"]);
-  userModel.User.findOne({ email: body.email }, (err, user) => {
-    if (err) {
-      res.send(err);
-    } else {
-      if (!user) {
-        res.send({ msg: "UserNotFound" });
-      } else if (user.password !== body.password) {
-        res.send({ msg: "notFound" });
-      } else {
-        res.send({ msg: "found", userName: user.userName });
-      }
-    }
-  });
-});
+// app.post("/login", (req, res) => {
+//   var body = _.pick(req.body, ["email", "password"]);
+//   userModel.User.findOne({ email: body.email }, (err, user) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       if (!user) {
+//         res.send({ msg: "UserNotFound" });
+//       } else if (user.password !== body.password) {
+//         res.send({ msg: "notFound" });
+//       } else {
+//         res.send({ msg: "found", userName: user.userName });
+//       }
+//     }
+//   });
+// });
 
-app.post("/register", (req, res) => {
-  var body = _.pick(req.body, ["email", "password", "username"]);
-  console.log(body);
-  userModel
-    .User({
-      email: body.email,
-      userName: body.username,
-      password: body.password
-    })
-    .save((err, user) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send("Registered");
-      }
-    });
-});
+// app.post("/register", (req, res) => {
+//   var body = _.pick(req.body, ["email", "password", "username"]);
+//   userModel
+//     .User({
+//       email: body.email,
+//       userName: body.username,
+//       password: body.password
+//     })
+//     .save((err, user) => {
+//       if (err) {
+//         res.send(err);
+//       } else {
+//         res.send("Registered");
+//       }
+//     });
+// });
 
-app.post("/checkUser", (req, res) => {
-  var body = _.pick(req.body, ["userName"]);
-  console.log(body);
-  userModel.User.findOne({ userName: body.userName }, (err, user) => {
-    if (err) {
-      res.send(err);
-    } else {
-      if (user) {
-        res.send("found");
-      } else {
-        res.send("notFound");
-      }
-    }
-  });
-});
+// app.post("/checkUser", (req, res) => {
+//   var body = _.pick(req.body, ["userName"]);
+//   userModel.User.findOne({ userName: body.userName }, (err, user) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       if (user) {
+//         res.send("found");
+//       } else {
+//         res.send("notFound");
+//       }
+//     }
+//   });
+// });
 
-app.post("/checkEmail", (req, res) => {
-  var body = _.pick(req.body, ["email"]);
-  console.log(body);
-  userModel.User.findOne({ email: body.email }, (err, user) => {
-    if (err) {
-      res.send(err);
-    } else {
-      if (user) {
-        res.send("found");
-      } else {
-        res.send("notFound");
-      }
-    }
-  });
-});
+// app.post("/checkEmail", (req, res) => {
+//   var body = _.pick(req.body, ["email"]);
+//   userModel.User.findOne({ email: body.email }, (err, user) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       if (user) {
+//         res.send("found");
+//       } else {
+//         res.send("notFound");
+//       }
+//     }
+//   });
+// });
 
 var IO = require("socket.io").listen(server);
 IO.on("connection", socket => {
   console.log("new User connected");
 
   socket.on("newMsg", message => {
-    console.log(message);
     IO.emit("message", {
       from: message.from,
       text: message.text,
-      createdAt: new Date(),
-      code: message.timeCode
+      createdAt: new Date().toLocaleTimeString()
+      //code: message.timeCode
     });
   });
 });
